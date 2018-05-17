bcd934d0afb70052ec41956b7f663e4b145adfc4
Maxi Bal <greatnesshere5@gmail.com>
2018-05-17 16:34:56 -0400

    Minor fix to web ui styling

--- src/public/js/playchar.js
+++ src/public/js/playchar.js
@@ -2,7 +2,11 @@ var obj = {0:"Alex",1:"Alfred",2:"Anita",3:"Anne",4:"Bernard",5:"Bill",6:"Charle
             10:"Frans",11:"George",12:"Herman",13:"Joe",14:"Maria",15:"Max",16:"Paul",17:"Peter",18:"Phillip",
             19:"Richard",20:"Robert",21:"Sam",22:"Susan",23:"Tom"
 }
-function processClick(charNum, connection) {
+function processClick(charNum, connection)
+{
     connection.send(obj[charNum], () => console.log("Sent data to websocket: " + obj[charNum]));
+    document.getElementById('RenderInActiveAfterRes').remove();
+    document.getElementById('Inactive').style.display = 'inline-block';
     return;
 }
+function guessClick(charNum, connection) {}
